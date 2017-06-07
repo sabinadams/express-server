291fdb4c4e9a0464090776fc51a3e1e269cc70f2
Sabin Adams <sabintheworld@gmail.com>
2017-06-07 10:28:11 -0700

    Authentication Working

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -9,21 +9,27 @@ var _authModel = require('../models/auth-model');
 
 module.exports = (req, res, next) => {
 
-    // Checks to see if the requested route is a protected route
-    if (whitelist.indexOf(req.url) == -1) {
-        let token = req.headers.auth.split('bearer ')[1];
-        _authModel.validateTokenSession(token, (session) => {
-            console.log(session)
-            if (session.logged_in) {
-                next();
-            } else {
-                res.send({ status: 401, message: 'Not Authorized' });
-            }
-        })
+    if (req.method == 'OPTIONS') {
+        return res.status(200).send({ message: 'Preflight check successful' });
     }
 
-    // This gets fired if the route was whitelisted (doesn't require authorization)
-    next();
+    // Checks to see if the requested route is a protected route
+    if (whitelist.indexOf(req.url) == -1) {
+
+        try {
+            let token = req.headers.auth.split('bearer ')[1];
+            _authModel.validateTokenSession(token, (session) => {
+                session.logged_in ? next() : res.send({ status: 401, message: 'Not Authorized' });
+            })
+        } catch (err) {
+            return res.status(200).send({ status: 401, message: 'Not Authorized' });
+        }
+
+    } else {
+        // This gets fired if the route was whitelisted (doesn't require authorization)
+        next();
+    }
+
 }
 
 // We could just check for a valid session here and not worry about whitelisted routes yet. 
